bf63e407da54a6bc50db0dc83865cb6710a0a08f
Shiluo <wushouyue@gmail.com>
2022-04-08 13:42:21 -0400

    Flask support static files

--- server.py
+++ server.py
@@ -2,7 +2,13 @@ import sys
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+
+
+@app.route("/")
+def index():
+    return app.send_static_file("index.html")
+
 
 if __name__ == '__main__':
     port = 8080
