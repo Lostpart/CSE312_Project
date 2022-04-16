c1764c332ec5339e293aa43c037cbee3b473f778
shen <sqi903447@gmail.com>
2022-04-16 19:15:56 -0400

    https://github.com/Lostpart/CSE312_Project/tree/main

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
 </template>
 
@@ -12,19 +8,11 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #0b0300;
+  margin-top: 100px;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+body{
+  margin:0;
+  background: rgb(246, 243, 243);
+} 
 </style>
---
+++ src/components/login.vue
@@ -0,0 +1,29 @@
+<template>
+  <form @submit.prevent ="handle"><img src="../assets/chat2.png" style ="float:right" alt="">
+      <input type="text" required placeholder="name" v-model="showName"> 
+      
+      <input type="password" required placeholder="password" v-model ="showpassword">
+     
+      <button>Login </button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    setup()
+    {
+        const showName = ref('')
+       const  showpassword = ref('')
+        const handle=()=>{
+            console.log(showName.value, showpassword.value)
+        }
+        return{showName, showpassword,handle}
+    }
+
+}
+</script>
+
+<style>
+
+</style>
