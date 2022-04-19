ae6809a47a922b4013430e4716d11aa591a8e241
Shiluo <wushouyue@gmail.com>
2022-04-18 20:23:07 -0400

    Add moment_dal.py

---
+++ dal/moment_dal.py
@@ -0,0 +1,16 @@
+from bson import ObjectId
+
+
+def insert(user_id: ObjectId, content: str, image_list, moment_collection):
+    data = {"from": user_id, "content": content}
+    if len(image_list) > 0:
+        data["image_list"] = image_list
+    return moment_collection.insert_one(data)
+
+
+def get_recent_moments(moment_collection, n=1000):
+    return moment_collection.find().sort({"_id": -1}).limit(n)
+
+
+def like_moment(moment_id: ObjectId, moment_collection):
+    return moment_collection.find_one_and_update({"_id": moment_id}, {"$inc": {"like": 1}})
