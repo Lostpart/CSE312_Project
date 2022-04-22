3aaad0c382020442b0853a99526650c5daa81364
Yanchao Wang <wyc935398521@gmail.com>
2022-04-22 12:20:45 -0400

    Signup & Login view style updated

--- 312_vue/src/components/LoginForm.vue
+++ 312_vue/src/components/LoginForm.vue
@@ -1,22 +1,20 @@
 <template>
-  <div @keyup.enter="login">
-    <v-form ref="form" v-model="valid" lazy-validation>
-      <v-text-field
-        v-model="email"
-        :rules="emailRules"
-        label="E-mail"
-        required
-      ></v-text-field>
+  <v-form ref="form" v-model="valid" lazy-validation>
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    ></v-text-field>
 
-      <v-text-field v-model="password" label="Password" required></v-text-field>
+    <v-text-field v-model="password" label="Password" required></v-text-field>
 
-      <v-btn :disabled="!valid" color="success" class="mr-4" @click="login">
-        LOGIN
-      </v-btn>
+    <v-btn :disabled="!valid" color="success" class="mr-4" @click="login">
+      LOGIN
+    </v-btn>
 
-      <v-btn color="error" class="mr-4" @click="reset"> Reset Form </v-btn>
-    </v-form>
-  </div>
+    <v-btn color="error" class="mr-4" @click="reset"> Reset Form </v-btn>
+  </v-form>
 </template>
 
 <script>
--- 312_vue/src/views/LoginView.vue
+++ 312_vue/src/views/LoginView.vue
@@ -1,7 +1,9 @@
 <template>
-    <v-card elevation="2" width="500" style="padding:30px">
+  <v-row justify="space-around">
+    <v-card elevation="2" width="500" style="padding: 30px; margin-top: 120px">
       <LoginForm></LoginForm>
     </v-card>
+  </v-row>
 </template>
 
 <script>
