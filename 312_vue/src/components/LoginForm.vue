b1b7880cbdc1ecf7ff9f62dfcac12e0f7e08334b
Yanchao Wang <wyc935398521@gmail.com>
2022-04-28 23:06:11 -0400

    Implemented dynamic username after login

--- 312_vue/src/components/LoginForm.vue
+++ 312_vue/src/components/LoginForm.vue
@@ -1,55 +1,47 @@
 <template>
-  <v-form ref="form" v-model="valid" lazy-validation>
-    <v-text-field
-      v-model="email"
-      :rules="emailRules"
-      label="E-mail"
-      required
-    ></v-text-field>
+	<v-form ref="form" v-model="valid" lazy-validation>
+		<v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
 
-    <v-text-field v-model="password" label="Password" required></v-text-field>
+		<v-text-field v-model="password" label="Password" required></v-text-field>
 
-    <v-btn :disabled="!valid" color="success" class="mr-4" @click="login">
-      LOGIN
-    </v-btn>
+		<v-btn :disabled="!valid" color="success" class="mr-4" @click="login"> LOGIN </v-btn>
 
-    <v-btn color="error" class="mr-4" @click="reset"> Reset Form </v-btn>
-  </v-form>
+		<v-btn color="error" class="mr-4" @click="reset"> Reset Form </v-btn>
+	</v-form>
 </template>
 
 <script>
-import axios from "axios";
+	import axios from 'axios'
 
-export default {
-  data: () => ({
-    valid: true,
-    email: "",
-    emailRules: [
-      (v) => !!v || "E-mail is required",
-      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
-    ],
-    password: "",
-  }),
+	export default {
+		data: () => ({
+			valid: true,
+			email: '1@12.com',
+			emailRules: [(v) => !!v || 'E-mail is required', (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid'],
+			password: '1',
+		}),
 
-  methods: {
-    login() {
-      const loginJson = {};
-      loginJson["email"] = this.email;
-      loginJson["password"] = this.password;
-      axios
-        .post("http://127.0.0.1:8080/login", loginJson)
-        .then((response) => {
-          if (response.data.status === "Error") {
-            alert(response.data["message"]);
-          } else {
-            alert(response.data["displayName"] + " logged in");
-          }
-        })
-        .catch((error) => alert(error));
-    },
-    reset() {
-      this.$refs.form.reset();
-    },
-  },
-};
+		methods: {
+			login() {
+				const loginJson = {}
+				loginJson['email'] = this.email
+				loginJson['password'] = this.password
+				axios
+					.post('http://127.0.0.1:8080/login', loginJson)
+					.then((response) => {
+						if (response.data.status === 'Error') {
+							alert(response.data['message'])
+						} else {
+							this.$store.commit('setDisplayName', response.data["displayName"])
+							this.$store.commit('setEmail', response.data["email"])
+							this.$store.commit('setUserID', response.data["user_id"])
+						}
+					})
+					.catch((error) => alert(error))
+			},
+			reset() {
+				this.$refs.form.reset()
+			},
+		},
+	}
 </script>
