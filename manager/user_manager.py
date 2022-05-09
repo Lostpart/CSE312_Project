bff05e2bb5e507ec8b1f5e2c58e87ad7241cc265
Shiluo <wushouyue@gmail.com>
2022-05-09 12:33:53 -0400

    Reformat code

--- manager/user_manager.py
+++ manager/user_manager.py
@@ -5,7 +5,7 @@ import json
 
 def login(email, password, user_collection):
     # Return true is the user doesn't exist, return user infomation if the user exist
-    message = user_dal.get_user(user_collection = user_collection, email = email, password = password)
+    message = user_dal.get_user(user_collection=user_collection, email=email, password=password)
     if not message["status"]:
         message["status"] = "Error"
         return json.dumps(message)
@@ -36,17 +36,21 @@ def register(username, email, password, user_collection):
         return json.dumps(userInfor)
     # return False
 
+
 def get_all_user(users_collection):
     """ Return user list in json format like this
         {{"displayName": displayName, "email": email, "user_id": id, "active": True/False}, .......}
     """
     return json.dumps(user_dal.retrieve_all(users_collection))
 
+
 def get_active_user(users_collection):
     return json.dumps(user_dal.retrieve_active_user(users_collection))
 
+
 def set_active(user_collection, user_id, status):
     return json.dumps(user_dal.update_user(user_collection, user_id, "active", status))
+
 
 def check_email_format(email: str):
     # Check if anything before @ is from a-z and 0-9 and if anything after @ is a-z and must have atleast 2 characters
@@ -62,6 +66,7 @@ def get_user_by_id(user_collection, id):
     return json.dumps(result)
     # return  user_dal.get_user_by_id(user_collection, id)
 
+
 def get_user_by_email(user_collection, email):
     result = user_dal.get_user(user_collection, email=email)
     if result["status"] == False:
@@ -70,8 +75,10 @@ def get_user_by_email(user_collection, email):
         result = result["message"]
     return json.dumps(result)
 
+
 def delete_user(user_collection, id):
     0
+
 
 def set_settings(user_collection, user_id, settings):
     user_dal.update_user(user_collection, user_id, "settings", settings)
